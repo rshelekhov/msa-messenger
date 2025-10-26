3f17fbe72a7ba431026383fac6298ecd079cbe93
Roman Shelekhov <shelekhoff@gmail.com>
2025-10-26 14:22:17 +0400

    feat: add gRPC services configuration to gateway
    - Introduced GRPCServices struct in the configuration to manage SSO, Chat, and Subscriber services.
    - Updated config_example.yaml to include settings for gRPC services, including addresses and verification URLs.

--- gateway/internal/config/config.go
+++ gateway/internal/config/config.go
@@ -1,9 +1,10 @@
 package config
 
 type Config struct {
-	App        App        `yaml:"App"`
-	HTTPServer HTTPServer `yaml:"HTTPServer"`
-	CORS       CORS       `yaml:"CORS"`
-	Validator  Validator  `yaml:"Validator"`
-	JWT        JWT        `yaml:"JWT"`
+	App          App          `yaml:"App"`
+	HTTPServer   HTTPServer   `yaml:"HTTPServer"`
+	CORS         CORS         `yaml:"CORS"`
+	Validator    Validator    `yaml:"Validator"`
+	JWT          JWT          `yaml:"JWT"`
+	GRPCServices GRPCServices `yaml:"GRPCServices"`
 }
